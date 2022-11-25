49d109789dc42fc1830ec604a9f0a101b0ea408d
reinhardjs <reinhardjsilalahi@gmail.com>
2022-11-25 21:59:56 +0700

    chore(simple-api): added role permission checker middleware

---
+++ middlewares/role_permisson_check.go
@@ -0,0 +1,53 @@
+package middlewares
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"simple-api/responses"
+)
+
+func contains(array []string, find string) bool {
+	for _, item := range array {
+		if item == find {
+			return true
+		}
+	}
+	return false
+}
+
+var RolePermissionCheck = func(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+
+		isPermitted := false
+
+		permittedEndpoints := map[string]map[string][]string{
+			"user": {
+				"GET": []string{"/token"},
+			},
+			"admin": {
+				"GET":  []string{"/token"},
+				"POST": []string{"/users"},
+			},
+		} //List of endpoints permitted to each role
+
+		requestPath := r.URL.Path //current request path
+
+		if contains(permittedEndpoints[UserRole][r.Method], requestPath) {
+			isPermitted = true
+		}
+
+		if !isPermitted { //Token is missing, returns with error code 403 Unauthorized
+			rw.Header().Add("Content-Type", "application/json")
+			rw.WriteHeader(http.StatusForbidden)
+			response := responses.BaseResponse{Status: http.StatusForbidden, Message: "You are not permitted to access this endpoint", Data: map[string]interface{}{}}
+			json.NewEncoder(rw).Encode(response)
+			return
+		}
+
+		//Everything went well, proceed with the request and set the caller to the user retrieved from the parsed token
+		ctx := context.WithValue(r.Context(), "is-role-permitted", true)
+		r = r.WithContext(ctx)
+		next.ServeHTTP(rw, r) //proceed in the middleware chain!
+	})
+}
