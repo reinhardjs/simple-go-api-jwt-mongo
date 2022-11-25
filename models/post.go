2aff1b99136b1e66b0ee017a3feb4be48845120a
reinhardjs <reinhardjsilalahi@gmail.com>
2022-11-26 01:50:31 +0700

    fix(simple-api): updated post model to display the ObjectID _id

--- models/post.go
+++ models/post.go
@@ -1,6 +1,9 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Post struct {
-	Title       string `json:"title,omitempty"`
-	Description string `json:"description,omitempty"`
+	Id          primitive.ObjectID `bson:"_id" json:"id,omitempty"`
+	Title       string             `json:"title,omitempty"`
+	Description string             `json:"description,omitempty"`
 }
