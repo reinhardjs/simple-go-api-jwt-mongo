7a45b58971b75f52344b19c89a3558c007d07566
reinhardjs <reinhardjsilalahi@gmail.com>
2022-11-25 18:37:33 +0700

    chore(simple-api): modify env key for token secret key

--- app/auth.go
+++ app/auth.go
@@ -51,7 +51,7 @@ var JwtAuthentication = func(next http.Handler) http.Handler {
 		tk := &models.Token{}
 
 		token, err := jwt.ParseWithClaims(tokenPart, tk, func(token *jwt.Token) (interface{}, error) {
-			return []byte(os.Getenv("token_password")), nil
+			return []byte(os.Getenv("token_secret_key")), nil
 		})
 
 		if err != nil { //Malformed token, returns with http code 403 as usual
