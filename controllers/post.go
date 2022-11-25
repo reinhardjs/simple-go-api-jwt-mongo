0a743ac21387a95127ebf63cc10a791738931f7a
reinhardjs <reinhardjsilalahi@gmail.com>
2022-11-25 23:54:00 +0700

    fix(simple-api): added get list post controller

--- controllers/post.go
+++ controllers/post.go
@@ -187,3 +187,43 @@ func GetPost() http.HandlerFunc {
 		json.NewEncoder(rw).Encode(response)
 	}
 }
+
+func GetAllPost() http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+		defer cancel()
+
+		rw.Header().Add("Content-Type", "application/json")
+
+		var posts []models.Post
+
+		//all validation passed, then insert new data to users collection
+		var postsCollection *mongo.Collection = configs.GetCollection(configs.DB, "posts")
+		results, err := postsCollection.Find(ctx, bson.M{})
+
+		if err != nil {
+			response := responses.BaseResponse{Status: http.StatusInternalServerError, Message: err.Error(), Data: map[string]interface{}{}}
+			rw.WriteHeader(response.Status)
+			json.NewEncoder(rw).Encode(response)
+			return
+		}
+
+		//reading from the db in an optimal way
+		defer results.Close(ctx)
+		for results.Next(ctx) {
+			var singlePost models.Post
+			if err = results.Decode(&singlePost); err != nil {
+				rw.WriteHeader(http.StatusInternalServerError)
+				response := responses.BaseResponse{Status: http.StatusInternalServerError, Message: err.Error(), Data: map[string]interface{}{}}
+				json.NewEncoder(rw).Encode(response)
+			}
+
+			posts = append(posts, singlePost)
+		}
+
+		response := responses.BaseResponse{Status: http.StatusOK, Message: "success", Data: map[string]interface{}{"results": posts}}
+		rw.WriteHeader(response.Status)
+		json.NewEncoder(rw).Encode(response)
+	}
+}
--- routes/post.go
+++ routes/post.go
@@ -8,6 +8,7 @@ import (
 
 func PostRoute(router *mux.Router) {
 	router.HandleFunc("/posts/{userId}", controllers.GetPost()).Methods("GET")
+	router.HandleFunc("/posts", controllers.GetAllPost()).Methods("GET")
 	router.HandleFunc("/posts", controllers.CreatePost()).Methods("POST")
 	router.HandleFunc("/posts/{userId}", controllers.UpdatePost()).Methods("PUT")
 	router.HandleFunc("/posts/{userId}", controllers.DeletePost()).Methods("DELETE")
