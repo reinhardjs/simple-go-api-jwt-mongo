e1b1854ac85458b4f1102e11b63d5c7e35d83ead
reinhardjs <reinhardjsilalahi@gmail.com>
2022-11-25 23:37:08 +0700

    fix(simple-api): refactor post model

--- models/post.go
+++ models/post.go
@@ -1,11 +1,6 @@
 package models
 
-import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
-
 type Post struct {
-	Id          primitive.ObjectID `json:"id,omitempty"`
-	Title       string             `json:"title,omitempty"`
-	Description string             `json:"description,omitempty"`
+	Title       string `json:"title,omitempty"`
+	Description string `json:"description,omitempty"`
 }
