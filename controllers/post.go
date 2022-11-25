51dd05534a9b71b97b4178f95ac1268a954be751
reinhardjs <reinhardjsilalahi@gmail.com>
2022-11-25 23:46:37 +0700

    fix(simple-api): added delete post controller

--- controllers/post.go
+++ controllers/post.go
@@ -110,3 +110,49 @@ func UpdatePost() http.HandlerFunc {
 		json.NewEncoder(rw).Encode(response)
 	}
 }
+
+func DeletePost() http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		params := mux.Vars(r)
+		userId := params["userId"]
+
+		defer cancel()
+
+		objId, _ := primitive.ObjectIDFromHex(userId)
+		var post models.Post
+
+		rw.Header().Add("Content-Type", "application/json")
+
+		//validate the request body
+		if err := json.NewDecoder(r.Body).Decode(&post); err != nil {
+			response := responses.BaseResponse{Status: http.StatusBadRequest, Message: "error", Data: map[string]interface{}{"error": err.Error()}}
+			rw.WriteHeader(response.Status)
+			json.NewEncoder(rw).Encode(response)
+			return
+		}
+
+		//use the validator library to validate required fields
+		if validationErr := utils.Validate.Struct(&post); validationErr != nil {
+			response := responses.BaseResponse{Status: http.StatusBadRequest, Message: validationErr.Error(), Data: map[string]interface{}{}}
+			rw.WriteHeader(response.Status)
+			json.NewEncoder(rw).Encode(response)
+			return
+		}
+
+		//all validation passed, then insert new data to users collection
+		var postsCollection *mongo.Collection = configs.GetCollection(configs.DB, "posts")
+		result, err := postsCollection.DeleteOne(ctx, bson.M{"_id": objId})
+
+		if err != nil {
+			response := responses.BaseResponse{Status: http.StatusInternalServerError, Message: err.Error(), Data: map[string]interface{}{}}
+			rw.WriteHeader(response.Status)
+			json.NewEncoder(rw).Encode(response)
+			return
+		}
+
+		response := responses.BaseResponse{Status: http.StatusOK, Message: "success", Data: result}
+		rw.WriteHeader(response.Status)
+		json.NewEncoder(rw).Encode(response)
+	}
+}
--- routes/post.go
+++ routes/post.go
@@ -9,4 +9,5 @@ import (
 func PostRoute(router *mux.Router) {
 	router.HandleFunc("/posts", controllers.CreatePost()).Methods("POST")
 	router.HandleFunc("/posts/{userId}", controllers.UpdatePost()).Methods("PUT")
+	router.HandleFunc("/posts/{userId}", controllers.DeletePost()).Methods("DELETE")
 }
